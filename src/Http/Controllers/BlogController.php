b5ee92ad8710175417cf0f1933ce41dbdbec914d
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-02-02 15:46:02 +0000

    Remove index and category routes for blogs and use repository class.

--- src/Http/Controllers/BlogController.php
+++ src/Http/Controllers/BlogController.php
@@ -5,21 +5,17 @@ namespace Creode\LaravelNovaBlog\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Routing\Controller;
 use Creode\LaravelNovaBlog\Entities\Post;
-use Creode\LaravelNovaBlog\Entities\PostCategory;
+use Creode\LaravelNovaBlog\Repositories\PostRepository;
 
 class BlogController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Constructor for class.
      *
-     * @return Renderable
+     * @param PostRepository $postRepository
      */
-    public function index()
+    public function __construct(protected PostRepository $postRepository)
     {
-        $postCategories = PostCategory::all();
-        $posts = Post::with('author', 'categories')->where('featured_post', '=', 0)->get();
-        $featuredPost = Post::with('author', 'categories')->where('featured_post', '=', 1)->first();
-        return view('nova-blog::index', compact('posts', 'featuredPost', 'postCategories'));
     }
 
     /**
@@ -30,14 +26,6 @@ class BlogController extends Controller
      */
     public function showPost(Post $post)
     {
-        $post->load('author', 'categories');
         return view('nova-blog::show', compact('post'));
-    }
-
-    public function showCategory(PostCategory $postCategory)
-    {
-        $postCategories = PostCategory::all();
-        $postCategory->load('posts');
-        return view('nova-blog::category', compact('postCategories', 'postCategory'));
     }
 }
